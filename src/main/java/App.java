3c7d3d52731978a5d09072c364905cd3e496bd30
Santhosh150900 <2022wa86885@wilp.bits-pilani.ac.in>
2026-09-24 06:03:44 +0530

    App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Continuous Integration");
+        System.out.println("Jenkins Continuous Integration Successfully Tested");
     }
 }
