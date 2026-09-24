a2eb9fc7978b567cd710d7ba4eb31964b52545f2
Santhosh.M <2022wa86885@wilp.bits-pilani.ac.in>
2026-09-24 06:10:09 +0530

    Modified App.java for CI testing

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -1,13 +1,7 @@
 package com.bits;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Jenkins Continuous Integration Successfully Tested");
     }
 }
